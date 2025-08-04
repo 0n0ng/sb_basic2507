ab6aefdad2eb8281b4bea2ab5761bdd84f8ed101
0n0ng <hy991006@gmail.com>
2025-08-04 15:23:26 +0900

    3-02, 페이지 이동기능 완성 및 최신순 데이터 조회

--- src/main/java/com/std/sbb/question/QuestionService.java
+++ src/main/java/com/std/sbb/question/QuestionService.java
@@ -2,12 +2,14 @@ package com.std.sbb.question;
 
 import com.std.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +24,10 @@ public class QuestionService {
     }
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 11);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+
         return this.questionRepository.findAll(pageable);
     }
 
